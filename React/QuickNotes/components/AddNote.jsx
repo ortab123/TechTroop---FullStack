14e62178d5579cbe2de2bb18a0c9c070f2b70b7b
ortab123 <131905006+ortab123@users.noreply.github.com>
2025-08-07 12:32:49 +0300

    Add date-fns and React Icons libraries. Step 2 accomplished

--- React/QuickNotes/components/AddNote.jsx
+++ React/QuickNotes/components/AddNote.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { format } from "date-fns";
+import { FaTimes } from "react-icons/fa";
 
 export default function AddNote() {
   const [noteText, setNoteText] = useState("");
@@ -9,11 +11,19 @@ export default function AddNote() {
 
     const newNote = {
       text: noteText,
-      date: new Date().toLocaleDateString("en-GB"),
+      date: format(new Date(), "MMM do h:mm a"),
     };
 
     setNotes([...notes, newNote]);
     setNoteText("");
+  };
+
+  const handleDeleteNote = (index) => {
+    const confirmDelete = confirm("Are you sure you want to delete your note?");
+    if (!confirmDelete) return;
+
+    const updatedNotes = notes.filter((_, i) => i !== index);
+    setNotes(updatedNotes);
   };
 
   return (
@@ -34,6 +44,12 @@ export default function AddNote() {
         {notes.map((note, index) => (
           <div key={index} className="note">
             <small>{note.date}</small>
+            <button
+              className="delete-btn"
+              onClick={() => handleDeleteNote(index)}
+            >
+              <FaTimes />
+            </button>
             <p>{note.text}</p>
           </div>
         ))}
